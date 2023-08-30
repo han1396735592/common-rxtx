769753aec1781a36fa98488876a8cc946eba0b96
han1396735592 <1396735592@qq.com>
2023-08-30 17:04:46 +0800

    StringSerialDataParser add charset config

--- src/main/java/cn/qqhxj/rxtx/parse/StringSerialDataParser.java
+++ src/main/java/cn/qqhxj/rxtx/parse/StringSerialDataParser.java
@@ -2,13 +2,30 @@ package cn.qqhxj.rxtx.parse;
 
 import cn.qqhxj.rxtx.context.SerialContext;
 
+import java.nio.charset.Charset;
+
 /**
  * @author han1396735592
  **/
 public class StringSerialDataParser implements SerialDataParser<String> {
+
+    private final Charset charset;
+
+    public StringSerialDataParser(Charset charset) {
+        this.charset = charset;
+    }
+
+    public StringSerialDataParser(String charset) {
+        this.charset = Charset.forName(charset);
+    }
+
+    public StringSerialDataParser() {
+        this(Charset.defaultCharset());
+    }
+
     @Override
     public String parse(byte[] bytes, SerialContext serialContext) {
-        return new String(bytes);
+        return new String(bytes, charset);
     }
 }
 
