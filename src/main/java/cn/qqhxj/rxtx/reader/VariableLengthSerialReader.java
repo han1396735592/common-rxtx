388b465359aca13d446de04b9a58ed5f8a3b2e1d
han1396735592 <1396735592@qq.com>
2023-08-30 10:39:15 +0800

    update VariableLengthSerialReader

--- src/main/java/cn/qqhxj/rxtx/reader/VariableLengthSerialReader.java
+++ src/main/java/cn/qqhxj/rxtx/reader/VariableLengthSerialReader.java
@@ -10,19 +10,25 @@ import java.util.Arrays;
  **/
 public class VariableLengthSerialReader extends BaseSerialReader {
 
-    private ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+    private final ByteBuffer byteBuffer;
 
 
-    private char startChar = '{';
+    private final char startChar;
 
-    private char endChar = '}';
+    private final char endChar;
 
     public VariableLengthSerialReader() {
+        this('{', '}');
     }
 
     public VariableLengthSerialReader(char startChar, char endChar) {
+        this(startChar, endChar, 1024);
+    }
+
+    public VariableLengthSerialReader(char startChar, char endChar, int length) {
         this.startChar = startChar;
         this.endChar = endChar;
+        this.byteBuffer = ByteBuffer.allocate(length);
     }
 
     @Override
@@ -32,7 +38,7 @@ public class VariableLengthSerialReader extends BaseSerialReader {
             try {
                 ch = this.getInputStream().read();
                 if (ch == startChar) {
-                    byteBuffer = ByteBuffer.allocate(1024);
+                    byteBuffer.position(0);
                     byteBuffer.put((byte) ch);
                     continue;
                 }
@@ -41,14 +47,16 @@ public class VariableLengthSerialReader extends BaseSerialReader {
                         if (((char) byteBuffer.get(0)) == startChar) {
                             byteBuffer.put((byte) ch);
                             byte[] array = Arrays.copyOf(byteBuffer.array(), byteBuffer.position());
-                            byteBuffer = ByteBuffer.allocate(1024);
+                            byteBuffer.position(0);
                             return array;
                         } else {
-                            byteBuffer = ByteBuffer.allocate(1024);
+                            byteBuffer.position(0);
                         }
                     }
                 }
-                byteBuffer.put((byte) ch);
+                if (ch != -1) {
+                    byteBuffer.put((byte) ch);
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
